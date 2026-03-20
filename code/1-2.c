fdf03472413a3a88357319d956201157d45f3b43
anixcn <anixcnu@gmail.com>
2026-03-20 09:37:43 +0900

    Update 1-2.c

---
+++ code/1-2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main() {
+    print(void);
+    int square(int n);
+
+    int num = 5;
+    int result = square(num);
+    printf("%d, %d\n", num, result);
+
+    return 0;
+}
+
+void print() {
+    printf("Welcome to Computer Programming 03 Lab!\n");
+}
+
+int square(int n) {
+    return n * n;
+}
