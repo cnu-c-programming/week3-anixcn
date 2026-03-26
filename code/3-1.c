3f526ed36bba2f27e375c1ebce425934f947ae9e
anixcn <anixcnu@gmail.com>
2026-03-26 23:57:52 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 
-void divide(int a, int b, int *q, int *r) {
-    *q = a / b;
-    *r = a % b;
+int call_count() {
+    static int count = 0;
+    return ++count;
 }
 
 int main() {
-    int a = 27;
-    int b = 4;
-    int q = 0;
-    int r = 0;
-
-    divide(a, b, &q, &r);
-
-    printf("%d\n%d\n", q, r);
+    printf("%d\n", call_count());
+    printf("%d\n", call_count());
+    printf("%d\n", call_count());
 
     return 0;
 }
